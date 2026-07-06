e397c991eaf7bc68c456b39c889946da59e2f3e4
smanecis <smanecis32@gmail.com>
2026-07-06 19:15:42 +0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+'https://hqescjoeudrdvvjwdhba.supabase.co';
+
+const SUPABASE_KEY =
+'sb_publishable_VyMFEHYu5QB22l5xcw3fuA_-5AkjVNv';
+
+const supabaseClient =
+supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
