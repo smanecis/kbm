acb9d298743c2b88d46e9b06d7f748d3d4cf57dd
smanecis <smanecis32@gmail.com>
2026-07-08 19:23:26 +0700

    Update jurnal.js

--- js/jurnal.js
+++ js/jurnal.js
@@ -65,3 +65,94 @@ async function loadJurnal(){
         )
         .innerHTML = html;
 }
+
+async function simpanJurnal(){
+
+    const jam_ke =
+        document.getElementById(
+            'jamKe'
+        ).value;
+
+    const mapel =
+        document.getElementById(
+            'mapel'
+        ).value;
+
+    const guru =
+        document.getElementById(
+            'guru'
+        ).value;
+
+    const jenis_kbm =
+        document.getElementById(
+            'jenis'
+        ).value;
+
+    const materi =
+        document.getElementById(
+            'materi'
+        ).value;
+
+    if (
+        jam_ke === '' ||
+        mapel === ''
+    ) {
+        alert(
+            'Lengkapi data.'
+        );
+        return;
+    }
+
+    const { error } =
+        await supabaseClient
+        .from('jurnal_kelas')
+        .upsert(
+            {
+                tanggal: hariIni,
+                kelas_id:
+                    user.kelas_id,
+                jam_ke,
+                mapel,
+                guru,
+                jenis_kbm,
+                materi,
+                petugas_id:
+                    user.id
+            },
+            {
+                onConflict:
+                    'tanggal,kelas_id,jam_ke'
+            }
+        );
+
+    if (error) {
+        alert(error.message);
+        return;
+    }
+
+    alert(
+        'Jurnal berhasil disimpan.'
+    );
+
+    document
+        .getElementById(
+            'jamKe'
+        ).value = '';
+
+    document
+        .getElementById(
+            'mapel'
+        ).value = '';
+
+    document
+        .getElementById(
+            'guru'
+        ).value = '';
+
+    document
+        .getElementById(
+            'materi'
+        ).value = '';
+
+    loadJurnal();
+}
